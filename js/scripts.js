21438562790c629ce2eda78a8d86f7eaf76cf78e
Shane Harper <cdtharper@gmail.com>
2020-08-10 22:43:42 -0400

    show letter spaces

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,34 @@
-let form = document.querySelector("#phrase-input form");
-let answer = document.querySelector("#phrase-input form input[name='the-answer']");
+const form = document.querySelector("#phrase-input form");
+const answerInput = document.querySelector("#phrase-input form input[name='the-answer']");
+const contestantInput = document.querySelector("#contestant-input");
+const submitBtn = document.querySelector("#submit-btn");
+const resetBtn = document.querySelector("#reset-btn");
+let answer;
+let answerSplit;
+let letterBlanks = "";
+
+resetBtn.setAttribute("disabled", "true");
 
 form.onsubmit = (e) => {
     e.preventDefault();
-    alert(answer.value);
+    answer = answerInput.value;
+    answerSplit = answer.split("");
+    console.log(answer, answerSplit);
+    answerSplit.forEach(el => {
+        if (el !== " ") {
+            letterBlanks += "<div class='word-block letter'></div>";
+        } else {
+            letterBlanks += "<div class='word-block space'></div>";
+        }
+    });
+    // console.log(letterBlanks);
+    contestantInput.innerHTML = letterBlanks;
+    submitBtn.setAttribute("disabled", "true");
+    resetBtn.setAttribute("disabled", "false");
+    answerInput.value = "";
 }
+
+resetBtn.addEventListener("click", () => {
+    submitBtn.setAttribute("disabled", "false");
+    resetBtn.setAttribute("disabled", "true");
+});
